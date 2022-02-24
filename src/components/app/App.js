8f4f7973f4d843dcf3a011a0bde87cd87b80d0bc
Rebecca Schechter <rschecht4@student.touro.edu>
2022-02-24 16:05:49 -0500

    Finished todo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -15,8 +15,8 @@ function App() {
 
   return (
     <div>
-       <Home />
-       {/* <Todo /> */}
+       {/* <Home /> */}
+       <Todo />
     </div>
   );
 }
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,32 +1,61 @@
 import React, {useState} from "react";
-import "./todo.css"
+import "./todo.css";
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import ClearIcon from '@mui/icons-material/Clear';
 
-export const Todo =(props)=> {
-    const[inputText, setInputText]= useState("");
-    const[todos, setTodos]= useState([]);
-     function addTodo(){
-        const newTodos =[...todos, inputText];
+export const Todo = () => {
+    const [inputText, setInputText] = useState('');
+    const [todos, setTodos] = useState([]);
+    
+    function addTodo(){
+        const newTodos = [...todos, {text: inputText, isChecked: false}];
         setTodos(newTodos);
-     }
-function deleteTodo(deletedTodo){
-    todos.filter(todo => todo !== deleteTodo)
+    }
 
-}
-    return ( 
-    <div>
-        <input type="text" onChange={(event) => setInputText(event.target.value)}  placeholder ="hi"/>
-            <button onClick ={addTodo}>Add</button>
-             {todos.map((todo) => (
-            <TodoItem text ={todo} deleteTodo={deleteTodo}/>
-            ))}
-            <div>inputText</div>
+    function deleteTodo(deletedTodo){
+        setTodos(todos.filter(todo => todo.text !== deletedTodo))
+    }
+    function setChecked(checkedTodo){
+        checkedTodo.isChecked = !checkedTodo.isChecked;
+    } 
+
+
+    return(
+        <div id="inputDiv">
+        <h1>To-Do List</h1>
+         
+        <input type= "text" onChange = {(event)=> setInputText(event.target.value)} placeholder="I need to..."/>
+        <button onClick={addTodo}> Add</button>
         
-    </div>
-);
-        };
-        const TodoItem = (props) =>{
-            return <div>{props.text}
-           <button onClick={() => props.deleteTodo(props.text)}>delete</button> </div>
+        {todos.map((todo) =>(
+             <ToDoItem text ={todo.text} deleteTodo ={deleteTodo}/>
+        ))}
 
-        };
+    </div>
+    );
+};
+
+const ToDoItem = (props) => { 
+   return <Grid  container spacing ={0} className = "grid">
+        <Grid item xs ={0}>
+        <input 
+         type = "checkbox"
+        name="text"
+        value=""
+        onChange={() => props.setIsChecked(props.text)}
+        />
+        </Grid>
+        <Grid item xs={1}>
+         {props.text}
+         </Grid>
+        <Grid item xs={1}>
+        <Button onClick={() => props.deleteTodo(props.text)}> <ClearIcon/> </Button>
+        </Grid>
+        </Grid>;
+    }
+
+
 export default Todo;
