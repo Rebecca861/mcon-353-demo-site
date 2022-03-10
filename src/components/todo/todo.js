3e8df89786bc39e0408790583b83613ca0f2f170
Rebecca Schechter <rschecht4@student.touro.edu>
2022-03-10 11:43:00 -0500

    Began working on saving todos' checked state, got node errors

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -9,6 +9,7 @@ import {todoContext} from '../app/App';
 import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { text } from "express";
 
 export const Todo = () => {
     const [inputText, setInputText] = useState('');
@@ -17,12 +18,43 @@ export const Todo = () => {
     function addTodo(){
         const newTodos = [...todos, {text: inputText, isChecked: false}];
         setTodos(newTodos);
-        setInputText(" ");
+        setInputText("");
     }
 
     function deleteTodo(deletedTodo){
-        setTodos(todos.filter(todo => todo.text !== deletedTodo))
+        setTodos(todos.filter(todo => todo.text !== deletedTodo));
     }
+
+    function checkOffTodo(setChecked){
+        setTodos(todos.forEach(todo => {if (todo.text === setChecked) {todo.isChecked = true;}}));
+        /*console.log(setChecked);
+        todos.forEach(todo => {
+            if (todo.text === setChecked) {
+                console.log("found it!");
+                todo = {text: setChecked, isChecked: true};
+                console.log("changed " + todo.text + " to setChecked " + todo.isChecked);
+                return;
+            }
+        });*/
+      }
+
+      function getIsChecked(todoText) {
+          todos.forEach(todo => {
+              if (todo.text === todoText) {
+                  return todo.isChecked;
+              }
+          })
+      }
+
+    //   function uncheckTodo(setUnchecked) {
+    //       todos.forEach(todo => {
+    //           if (todo.text === setUnchecked) {
+    //               todo.isChecked = false;
+    //               return;
+    //           }
+    //       });
+    //   }
+
 
 
     return(
@@ -34,7 +66,7 @@ export const Todo = () => {
         <Button variant="contained" onClick={addTodo} size="large">Add</Button>
         
         {todos.map((todo) =>(
-             <ToDoItem text ={todo.text} deleteTodo ={deleteTodo}/>
+             <ToDoItem getIsChecked ={getIsChecked} text ={todo.text} isChecked = {(text)=getIsChecked(text)} deleteTodo ={deleteTodo} checkOffTodo ={checkOffTodo}/>
         ))}
 
     </div>
@@ -44,13 +76,15 @@ export const Todo = () => {
 const ToDoItem = (props) => { 
 
     const label = { inputProps: { 'aria-label': '' } };
+    const isChecked = props.getIsChecked(props.text);
 
    return <Grid  container spacing ={0} className = "grid">
         <Grid item xs ={0}>
         <Checkbox {...label} color="secondary" 
         name="text"
         value=""
-        onChange={() => props.setChecked(props.text)}/>
+        onChange={() => props.checkOffTodo(props.text)}
+        checked = {isChecked}/>
         </Grid>
         <Grid item xs={1}>
          {props.text}
