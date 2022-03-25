939d0e67335a120339c4faff10355dc5860921c6
Rebecca Schechter <rschecht4@student.touro.edu>
2022-03-25 14:23:17 -0400

    Finished chat page, added github pages dependencies

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,23 +1,18 @@
-import './App.css';
-import React, {useState} from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Rating from '@mui/material/Rating'
-import Home from '../home/home'
-import {Todo} from '../todo/todo'
-import {Header} from '../header/header'
-import {Chat} from '../chat/chat'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import {TodoProvider} from "../../state/todo/context"
-
+import "./App.css";
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+import Home from "../home/home";
+import { Todo } from "../todo/todo";
+import { Header } from "../header/header";
+import { Chat } from "../chat/chat";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { TodoProvider } from "../../state/todo/context";
 
 //const todoList = {};
 
@@ -34,33 +29,21 @@ import {TodoProvider} from "../../state/todo/context"
   }
 };*/
 
-
-
-
-
-
 function App() {
-  
-  
-  
-
-  
-
   return (
     <div>
       <TodoProvider>
-      <BrowserRouter>
-      <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>  
-          <Route path = "/todo" element={<Todo />}></Route>
-          <Route path="/chat" element={<Chat />}></Route>
-        </Routes>
-      </BrowserRouter>
+        <HashRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/todo" element={<Todo />}></Route>
+            <Route path="/chat" element={<Chat />}></Route>
+          </Routes>
+        </HashRouter>
       </TodoProvider>
-
     </div>
   );
-  }
+}
 
 export default App;
--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,108 +1,237 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
-import { styled } from "@mui/material/styles";
+import { styled as styled1 } from "@mui/material/styles";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-
+import CustomSelect from "@mui/material/Select";
+import PropTypes from "prop-types";
+import SelectUnstyled, {
+  selectUnstyledClasses,
+} from "@mui/base/SelectUnstyled";
+import OptionUnstyled, {
+  optionUnstyledClasses,
+} from "@mui/base/OptionUnstyled";
+import PopperUnstyled from "@mui/base/PopperUnstyled";
+import { styled as styled2 } from "@mui/system";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import AddSharpIcon from "@mui/icons-material/AddSharp";
 export const Chat = () => {
   const [chats, setChats] = useState([
-    { chatId: 1, name: "Chat 1" },
-    { chatId: 2, name: "Chat 2" },
+    // { chatId: "18fa1c30-c741-4fa2-9b02-190f3dfcfccc", name: "Chat 1" },
+    // { chatId: "18fa1c30-c741-4fa2-9b02-190f3dfcfccc", name: "Chat 2" },
   ]);
+
+  const [chat, setChat] = useState({
+    // id: "18fa1c30-c741-4fa2-9b02-190f3dfcfccc",
+    // name: "demoSiteChat",
+  });
+
   const [chatMessages, setChatMessages] = useState([]);
   const [message, setMessage] = useState("");
 
-  const [alignment, setAlignment] = useState("chat1");
+  const [userName, setUserName] = useState("");
+
+  const [newChatName, setNewChatName] = useState("");
 
   const handleChange = (event, newAlignment) => {
-    setAlignment(newAlignment);
-    setChatMessages([]);
+    setChat(event.target.value);
+    console.log("val: " + event.target.value);
+    //setChatMessages([]);
   };
 
-  //     const MINUTE_MS = 6000000;
+  function getChats() {
+    fetch("https://z36h06gqg7.execute-api.us-east-1.amazonaws.com/chats", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setChats(data.Items));
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     getChats();
-  //   }, MINUTE_MS);
-
-  //   return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
-  // }, [])
+    console.log("In get chats, chats: ");
+    chats.forEach((c) => console.log(c.chatId));
+  }
 
   function addChatMessage() {
-    // const chatMessage = {
-    //     chatId: "18fa1c30-c741-4fa2-9b02-190f3dfcfccc",
-    //     userName: "demoSiteUser",
-    //     text: message
-    // };
+    const chatMessage = {
+      chatId: chat.id,
+      username: userName,
+      text: message,
+    };
 
-    // fetch('https://z36h06gqg7.execute-api.us-east-1.amazonaws.com/messages', {
-    //     method: "PUT",
-    //     headers: {
-    //       "Content-Type": "application/json", // tells REST that we will send the body data in JSON format
-    //     },
-    //     body: JSON.stringify(chatMessage),
-    //   }).then((response) => response.json()) // pull the json out of the response
-    //   .then((data) => console.log(data));
+    fetch("https://z36h06gqg7.execute-api.us-east-1.amazonaws.com/messages", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json", // tells REST that we will send the body data in JSON format
+      },
+      body: JSON.stringify(chatMessage),
+    })
+      .then((response) => response.json()) // pull the json out of the response
+      .then((data) => console.log(data));
 
-    setChatMessages([...chatMessages, { text: message }]);
+    //setChatMessages([...chatMessages, { text: message }]);
   }
 
-  function getChats() {
-    //         fetch("https://z36h06gqg7.execute-api.us-east-1.amazonaws.com/chats/18fa1c30-c741-4fa2-9b02-190f3dfcfccc/messages")
-    //   .then((response) => response.json())
-    //   .then((data) =>console.log(data))
-    // console.log("chats: " + chatMessages);
+  function addChat() {
+    const chat = {
+      name: newChatName,
+    };
+
+    fetch("https://z36h06gqg7.execute-api.us-east-1.amazonaws.com/chats", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(chat),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
+
+  function useInterval(callback, delay, ...callbackParams) {
+    const savedCallback = useRef();
+
+    // remember the latest callback
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    // setup the interval
+    useEffect(() => {
+      function tick() {
+        savedCallback.current(callbackParams);
+      }
+
+      if (delay !== null) {
+        const id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [callback, delay, callbackParams]);
+  }
+
+  useInterval(
+    (params) => {
+      const chatId = chat.id; //params[0];
+      fetch(
+        `https://z36h06gqg7.execute-api.us-east-1.amazonaws.com/chats/${chatId}/messages`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setChatMessages(data.Items);
+        });
+    },
+    1000, // fast polling
+    //60000, // slow polling
+    chat.id
+  );
+
+  // useInterval((params) => {
+  //   fetch(`https://z36h06gqg7.execute-api.us-east-1.amazonaws.com/chats`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setChats(data.Items);
+  //     });
+  // }, 1000);
+
+  // function getChatMessages() {
+  //   fetch(
+  //     "https://z36h06gqg7.execute-api.us-east-1.amazonaws.com/chats/18fa1c30-c741-4fa2-9b02-190f3dfcfccc/messages"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => setChatMessages(data.Items));
+  //   console.log("chats: " + chatMessages);
+  // }
 
   return (
     <div id="chatDiv">
       <h1>Chat!</h1>
-      <ToggleButtonGroup
-        color="primary"
-        value={alignment}
-        exclusive
-        onChange={handleChange}
-      >
-        <ToggleButton value="chat1">Chat 1</ToggleButton>
-        <ToggleButton value="chat2">Chat 2</ToggleButton>
-      </ToggleButtonGroup>
-      <br />
-      <br />
-      {/* <div id="chatBody">
-                <table>
-                    <tr>
-                        <td>Here is a chat</td>
-                    </tr>
-                </table>
-            </div> */}
-      <TextField
-        id="outlined-basic"
-        label="Enter your message here..."
-        multiline
-        variant="outlined"
-        onChange={(event) => setMessage(event.target.value)}
-      />
-      <br />
-      <br />
-      <Button variant="contained" onClick={addChatMessage} size="large">
-        Post
-      </Button>
+
+      <FormControl sx={{ minWidth: 120 }}>
+        <div>
+          <InputLabel id="demo-select-label">Select a chat...</InputLabel>
+          <Select
+            labelId="demo-select-label"
+            id="demo-select"
+            //value={chat}
+            onChange={handleChange}
+            onOpen={getChats}
+            label="Select a chat..."
+            style={{ minWidth: 250 }}
+            //MenuProps={MenuProps}
+          >
+            {chats.map((chat) => (
+              <MenuItem
+                key={chat.chatId}
+                value={chat}
+                //style={getStyles(name, personName, theme)}
+              >
+                {chat.name}
+              </MenuItem>
+            ))}
+          </Select>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; OR&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          {/* <br />
+        <br /> */}
+          <TextField
+            id="outlined-basic"
+            label="Create new chat..."
+            multiline
+            variant="outlined"
+            onChange={(event) => setNewChatName(event.target.value)}
+          />
+          &nbsp;&nbsp;
+          <Button variant="contained" onClick={addChat} size="large">
+            <AddSharpIcon />
+          </Button>
+        </div>
+
+        <br />
+
+        <TextField
+          id="outlined-basic"
+          label="Enter your username here..."
+          multiline
+          variant="outlined"
+          onChange={(event) => setUserName(event.target.value)}
+        />
+
+        <br />
+        <TextField
+          id="outlined-basic"
+          label="Enter your message here..."
+          multiline
+          variant="outlined"
+          onChange={(event) => setMessage(event.target.value)}
+        />
+
+        <br />
+        <br />
+        <Button variant="contained" onClick={addChatMessage} size="large">
+          Post
+        </Button>
+      </FormControl>
 
       {chatMessages.map((chatMessage) => (
-        <ChatItem text={chatMessage.text} />
+        <ChatMessageItem
+          text={chatMessage.text}
+          userName={chatMessage.username}
+        />
       ))}
     </div>
   );
 };
 
-const ChatItem = (props) => {
-  const Item = styled(Paper)(({ theme }) => ({
+const ChatMessageItem = (props) => {
+  const Item = styled1(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(1),
@@ -112,7 +241,7 @@ const ChatItem = (props) => {
 
   return (
     <Stack spacing={2} className="grid">
-      <Item>{props.text}</Item>
+      <Item>{props.userName + ": " + props.text}</Item>
       <br />
     </Stack>
   );
